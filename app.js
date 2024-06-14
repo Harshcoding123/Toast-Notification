413da94c7817253789c37948dec049836fc14065
Harsh Chouksey <133229856+Harshcoding123@users.noreply.github.com>
2024-06-14 17:00:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,33 +8,10 @@ icon1.classList.add('fa-solid', 'fa-circle-check');
     let msg1 = '<i class="fa-solid fa-circle-check fa-xl" style="color: #146713;"></i> Successfully Submitted';
     let msg2= ' <i class="fa-solid fa-circle-xmark fa-xl" style="color: #db0606;"></i>  Please Fix the Error';
     let msg3 =' <i class="fa-solid fa-circle-exclamation fa-xl" style="color: #FFD43B;"></i> Invalid input check again';
-// let i= document.querySelector("i");
-// let t1= document.querySelector("#t1");
-// t1.remove();
 
-// i.remove();
-// icon1.setAttribute("class","fa-solid fa-circle-check");
-    // icon1.classList.add('fa-solid', 'fa-circle-check');
-
-    // icon1.style.color = "#146713"
 
 let toastBox= document.querySelector("#toastBox");
-// box1.addEventListener("click",()=>{
-//     let t1= document.createElement("div");
-//     let p1= document.createElement("p");
-//      icon1.classList.add('fa-solid', 'fa-circle-check');
-//     icon1.style.color = "#146713"
-//     p1.innerText= "Successfully Submitted";
-//     t1.setAttribute("id","t1");
-//     t1.prepend(icon1);
-//     t1.setAttribute("class","toast");
-//       t1.append(p1);
-   
-//     toastBox.append(t1);
-//     setTimeout(()=>{
-//         t1.remove();
-//     },2000)
-// })
+
 box1.addEventListener("click",()=>{
     showToast(msg1);
 })
@@ -61,10 +38,5 @@ function showToast(msg){
         t1.remove();
     },2000)
 }
-// boxes.forEach((event)=>{
-//     event.addEventListener("click",(e)=>{
-//         showToast();
 
-// });
-// });
 
